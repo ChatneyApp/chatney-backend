b376fced3cc8d7359e219dfa2fca1aaa41cc247b
Denis Klyuev <denis.klyuev@gmail.com>
2026-03-18 22:21:22 -0400

    feat: seed base role data

--- ChatneyBackend/Infra/Migrations/_202508180001_CreateRolesTable.cs
+++ ChatneyBackend/Infra/Migrations/_202508180001_CreateRolesTable.cs
@@ -1,4 +1,10 @@
+using ChatneyBackend.Domains.Channels;
+using ChatneyBackend.Domains.Messages;
+using ChatneyBackend.Domains.Roles;
+using ChatneyBackend.Domains.Users;
+using ChatneyBackend.Domains.Workspaces;
 using FluentMigrator;
+using Npgsql;
 
 namespace ChatneyBackend.Infra.Migrations;
 
@@ -7,16 +13,55 @@ public class _202508180001_CreateRolesTable : Migration
 {
     public override void Up()
     {
-        Execute.Sql("""
-            CREATE TABLE IF NOT EXISTS roles (
-                id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                name varchar(255) NOT NULL,
-                is_base bool NOT NULL DEFAULT false,
-                permissions text[] NOT NULL DEFAULT '{}',
-                created_at timestamptz NOT NULL DEFAULT NOW(),
-                updated_at timestamptz NOT NULL DEFAULT NOW()
-            );
-        """);
+        Execute.WithConnection((connection, _) =>
+        {
+            using var command = new NpgsqlCommand
+            {
+                Connection = (NpgsqlConnection)connection
+            };
+            command.CommandText = """
+                CREATE TABLE IF NOT EXISTS roles (
+                    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                    name varchar(255) NOT NULL,
+                    is_base bool NOT NULL DEFAULT false,
+                    permissions text[] NOT NULL DEFAULT '{}',
+                    created_at timestamptz NOT NULL DEFAULT NOW(),
+                    updated_at timestamptz NOT NULL DEFAULT NOW()
+                );
+
+                INSERT INTO roles (name, is_base, permissions)
+                SELECT @name, TRUE, @permissions
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM roles
+                    WHERE name = @name
+                      AND is_base = TRUE
+                );
+            """;
+            command.Parameters.AddRange(new NpgsqlParameter[]
+            {
+                new NpgsqlParameter<string>("name", ChatneyBackend.Domains.Roles.DomainSettings.BaseRoleName),
+                new NpgsqlParameter<string[]>("permissions", [
+                    MessagePermissions.CreateMessage,
+                    MessagePermissions.DeleteMessage,
+                    MessagePermissions.EditMessage,
+                    MessagePermissions.ReadMessage,
+                    UserPermissions.ReadUser,
+                    UserPermissions.EditUser,
+                    ChannelPermissions.CreateMessage,
+                    ChannelPermissions.DeleteMessage,
+                    ChannelPermissions.EditMessage,
+                    ChannelPermissions.ReadChannel,
+                    ChannelPermissions.ReadMessage,
+                    WorkspacePermissions.ReadWorkspace
+                ])
+                {
+                    NpgsqlDbType = NpgsqlTypes.NpgsqlDbType.Array | NpgsqlTypes.NpgsqlDbType.Text
+                }
+            });
+
+            command.ExecuteNonQuery();
+        });
     }
 
     public override void Down()
